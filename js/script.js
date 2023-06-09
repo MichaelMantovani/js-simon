449460363f6ed3b694b1bf0b6d4ee51f6fd1592d
MichaelMantovani <michmantovani@gmail.com>
2023-06-09 17:18:01 +0200

    Collect DOM elements

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log('JS OK')
+
+// OPERAZIONI PRELIMINARI
+
+// Recupero gli elementi dal DOM
+const countdown = document.getElementById('countdown');
+const gameMode = document.getElementById('game-mode');
+const playButton = document.getElementById('play-btn')
+const toGuessNumbers = document.getElementById('to-guess-numbers');
+const guessNumbers = document.getElementById('guess-number');
+const guessInput = document.getElementById('guess-input');
+const scoreButton = document.getElementById('btn-score');
+const resultPlaceholder = document.getElementById('result');
