9e3f9ee0fc18346aa2327cd168dbdde1d21df00d
MichaelMantovani <michmantovani@gmail.com>
2023-06-09 17:22:31 +0200

    Create getRandomNumber function

--- js/script.js
+++ js/script.js
@@ -11,3 +11,9 @@ const guessNumbers = document.getElementById('guess-number');
 const guessInput = document.getElementById('guess-input');
 const scoreButton = document.getElementById('btn-score');
 const resultPlaceholder = document.getElementById('result');
+
+// Creo un funzione che mi genera numeri casuali da un minimo ad un massimo prestabiliti
+const getRandomNumber = (min , max) => {
+    const randomNumber = Math.floor(Math.random() * (max - min) + min);
+    return randomNumber;
+}
